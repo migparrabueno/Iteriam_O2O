ba25e8f4a9fd5b9d6903dd75f3f4f93f10c2ae15
Miguel Parra Bueno <52406968+migparrabueno@users.noreply.github.com>
2022-09-06 14:02:57 +0200

    Cambio de css a scss

--- src/app/home/components/home.component.ts
+++ src/app/home/components/home.component.ts
@@ -7,7 +7,7 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
